527fc24913d98e0adc553928859e2d5c810762ae
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-29 15:40:47 -0400

    Update Tree_Depth.java

--- Tree_Depth/src/Tree_Depth/Tree_Depth.java
+++ Tree_Depth/src/Tree_Depth/Tree_Depth.java
@@ -1,4 +1,38 @@
 package Tree_Depth;
 
+import java.util.Stack;
+
 public class Tree_Depth {
+    public int Tree_Depth(TreeNode root){
+        if(root == null) {
+            return 0;
+        }
+        int max_depth = 1;
+
+        Stack<TreeNode> nodes = new Stack<>();
+        Stack<Integer> depth = new Stack<>();
+
+        nodes.push(root);
+        depth.push(1);
+
+        while(!nodes.empty()){
+            TreeNode pres = nodes.pop();
+            int d = depth.pop();
+
+            if(pres.right == null && pres.left == null){
+                max_depth = Math.max(max_depth, d);
+            }
+
+            if(pres.right != null){
+                nodes.push(pres.right);
+                depth.push(d + 1);
+            }
+            if(pres.left != null){
+                nodes.push(pres.left);
+                depth.push(d+1);
+            }
+        }
+        return max_depth;
+    }
+
 }
